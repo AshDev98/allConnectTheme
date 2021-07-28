8c4e6a37d3a8e2aab695ddbea6ee7a36672c854f
naufalfathur <naufalfathur@gmail.com>
2021-07-28 02:24:18 +0000

    fix navbar routing

--- naufaltheme/public/js/naufaltheme.js
+++ naufaltheme/public/js/naufaltheme.js
@@ -122,7 +122,7 @@ function addScript(src) {
 
 
 window.onload = function(){
-
+  document.querySelector('a.navbar-brand.navbar-home') == null ? console.log("") : document.querySelector('a.navbar-brand.navbar-home').addEventListener("click", redirect.bind(null, url+"/app"));
   
   const login = document.querySelector('.for-login .page-card-head img');
   console.log(login);
